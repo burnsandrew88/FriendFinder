024db2be329b52e2928bf39251013a829c76be0b
Andrew Burns <andrewburns@Andrews-MacBook-Air.local>
2018-12-21 17:16:20 -0600

    User Input is logging to the API Aray

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,34 +2,34 @@
 var friends = [
     {
         friendName: "Detective Jake Peralta",
-        Image:"",
-        answers: []
+        Image:"https://vignette.wikia.nocookie.net/brooklynnine-nine/images/7/7f/JakeS5.jpg/revision/latest?cb=20171103222710",
+        scores: [5,5,5,5,5,5,5,5,5,5]
 
     }, {
         friendName: "Detective Charles Boyle",
-        Image:"",
-        answers: []
+        Image:"https://vignette.wikia.nocookie.net/brooklynnine-nine/images/c/c5/BoyleS5.jpg/revision/latest?cb=20171103224410",
+        scores: [5,5,5,5,5,5,5,5,5,5]
     }, {
         friendName: "Detective Amy Santiago",
-        Image:"",
-        answers: []
+        Image:"https://i.pinimg.com/originals/10/f8/b7/10f8b7f0495b2f5eca74ed778f0393a3.jpg",
+        scores: [5,5,5,5,5,5,5,5,5,5]
     } , {
         friendName: "Detective Rosa Diaz",
-        Image:"",
-        answers: []
+        Image:"https://vignette.wikia.nocookie.net/brooklynnine-nine/images/7/76/RosaS5.jpg/revision/latest?cb=20171103224359",
+        scores: [5,5,5,5,5,5,5,5,5,5]
     }, {
         friendName: "Sergeant Terry Jeffords",
-        Image:"",
-        answers: []
+        Image:"https://vignette.wikia.nocookie.net/brooklynnine-nine/images/f/fd/TerryS5.jpg/revision/latest?cb=20171103223830",
+        scores: [5,5,5,5,5,5,5,5,5,5]
 
     },{
         friendName:"Captain Ray Holt",
-        Image:"",
-        answers: []
+        Image:"https://tribzap2it.files.wordpress.com/2015/05/andre-braugher-brooklyn-nine-nine-season-2-finale-fox.jpg?w=900&h=506",
+        scores: [5,5,5,5,5,5,5,5,5,5]
     },{
         friendName: "Gina Linetti",
-        Image:"",
-        answers:[]
+        Image:"https://i.pinimg.com/736x/af/b8/12/afb8127347633e5017b8809ea82b7876.jpg",
+        scores:[5,5,5,5,5,5,5,5,5,5]
     }
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,22 +7,23 @@ module.exports = function(app){
         res.json(friendsData);
     });
     app.post("/api/friends", function(req,res){
-        console.log(req.body.answers);
+        res.json(req.body.scores);
 
     // Take in the user details (name, picture, answers)
-    var user = req.body;
+    var newUser = req.body;
+    console.log(newUser);
 
-    for (var i = 0; i < user.answers.length;i++){
-        user.answers[i] = parseInt(user.answers[i]);
+    for (var i = 0; i < newUser.scores.length;i++){
+        newUser.scores[i] = parseInt(newUser.scores[i]);
     }
     // establish how to choose between chosen friend and total difference between user and arry of friends 
     var chosenFriendIndex = 0;
     var minimumDifference = 40;
 
-    for (var x = 0; x < friends.length; x++){
+    for (var i = 0; i < friendsData.length; i++){
         var totalDifference = 0;
-        for(var z = 0; z < friends.answers.length; z++ ){
-                var difference = Math.abs(user.answers[z]- friends[x].answers[z]);
+        for(var z = 0; z < friendsData[i].scores.length; z++ ){
+                var difference = Math.abs(newUser.scores[z]- friendsData[i].scores[z]);
                 totalDifference += difference;
         }
 
@@ -32,10 +33,10 @@ module.exports = function(app){
         }
     }
     // add our new user to arry
-    friends.push(user);
+    friendsData.push(newUser);
 
     // returns back to browser
 
-    res.json(friends[chosenFriendIndex]);
+    res.json(friendsData[chosenFriendIndex]);
     });
     };
