f8b0dbd37c75eeeb0f72437b68e8004c55b2bd30
Andrew Burns <andrewburns@Andrews-MacBook-Air.local>
2018-12-22 16:04:47 -0600

    Finished App

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,33 +3,33 @@ var friends = [
     {
         friendName: "Detective Jake Peralta",
         Image:"https://vignette.wikia.nocookie.net/brooklynnine-nine/images/7/7f/JakeS5.jpg/revision/latest?cb=20171103222710",
-        scores: [5,5,5,5,5,5,5,5,5,5]
+        scores: [5,2,1,3,5,3,2,3,1,5]
 
     }, {
         friendName: "Detective Charles Boyle",
         Image:"https://vignette.wikia.nocookie.net/brooklynnine-nine/images/c/c5/BoyleS5.jpg/revision/latest?cb=20171103224410",
-        scores: [5,5,5,5,5,5,5,5,5,5]
+        scores: [3,4,3,5,2,2,5,4,2,3]
     }, {
         friendName: "Detective Amy Santiago",
         Image:"https://i.pinimg.com/originals/10/f8/b7/10f8b7f0495b2f5eca74ed778f0393a3.jpg",
-        scores: [5,5,5,5,5,5,5,5,5,5]
+        scores: [2,5,4,2,1,2,2,1,4,3]
     } , {
         friendName: "Detective Rosa Diaz",
         Image:"https://vignette.wikia.nocookie.net/brooklynnine-nine/images/7/76/RosaS5.jpg/revision/latest?cb=20171103224359",
-        scores: [5,5,5,5,5,5,5,5,5,5]
+        scores: [5,1,1,1,5,4,1,2,1,4]
     }, {
         friendName: "Sergeant Terry Jeffords",
         Image:"https://vignette.wikia.nocookie.net/brooklynnine-nine/images/f/fd/TerryS5.jpg/revision/latest?cb=20171103223830",
-        scores: [5,5,5,5,5,5,5,5,5,5]
+        scores: [4,1,1,4,5,2,2,3,1,5]
 
     },{
         friendName:"Captain Ray Holt",
         Image:"https://tribzap2it.files.wordpress.com/2015/05/andre-braugher-brooklyn-nine-nine-season-2-finale-fox.jpg?w=900&h=506",
-        scores: [5,5,5,5,5,5,5,5,5,5]
+        scores: [1,5,5,2,2,2,2,2,5,5]
     },{
         friendName: "Gina Linetti",
         Image:"https://i.pinimg.com/736x/af/b8/12/afb8127347633e5017b8809ea82b7876.jpg",
-        scores:[5,5,5,5,5,5,5,5,5,5]
+        scores:[1,1,1,3,2,2,1,5,1,3]
     }
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,15 +7,13 @@ module.exports = function(app){
         res.json(friends);
     });
     app.post("/api/friends", function(req,res){
-        res.json(req.body.scores);
+        // res.json(req.body.scores);
 
     // Take in the user details (name, picture, answers)
     var newUser = req.body;
     console.log(newUser);
     var userResponse = newUser.scores;
     var match = {
-        friendName: "",
-        Image: "",
         difference: 500
     };
 
@@ -24,7 +22,7 @@ module.exports = function(app){
     for (var i = 0; i < friends.length; i++){
         var totalDifference = 0;
         for(var z = 0; z < userResponse.length; z++ ){
-                totalDifference += Math.abs(friends[i].scores[z]- userResponse[z]);
+                totalDifference += Math.abs(parseInt(friends[i].scores[z])- parseInt(userResponse[z]));
         }
 
         if(totalDifference <= match.difference){
@@ -33,11 +31,14 @@ module.exports = function(app){
             match.difference = totalDifference
         }
     }
-    // add our new user to arry
+    
+    // push to the arry 
     friends.push(newUser);
 
     // returns back to browser
 
     res.json(match);
+
+   
     });
     };
